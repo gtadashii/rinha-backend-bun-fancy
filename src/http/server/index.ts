e71633d9e6920b27da2595592580c7535c3ca68b
Guilherme Tadashii <guilhermetadashii.dev@gmail.com>
2023-10-01 19:07:26 -0300

    fix: function call on routes

--- src/http/server/index.ts
+++ src/http/server/index.ts
@@ -1,28 +1,49 @@
 import 'reflect-metadata';
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia';
 import PersonsController from '../../controllers/persons.controller';
-import { createPersonSchema, retrievePersonByIdSchema, searchPersonByTermSchema } from '../validations/person.schema.validation';
+import {
+  createPersonSchema,
+  retrievePersonByIdSchema,
+  searchPersonByTermSchema,
+} from '../validations/person.schema.validation';
 import PostgresDataSource from '../../database/typeorm/datasource';
 
-const SERVER_PORT = process.env.HTTP_PORT;
+const SERVER_PORT = process.env.HTTP_PORT as unknown as number;
 
-PostgresDataSource.initialize()
-  .then(() => {
-    console.log("Data Source has been initialized!")
-  })
-  .catch((err) => {
-    console.error("Error during Data Source initialization", err)
-  });
+await PostgresDataSource.initialize();
 
 const personController = new PersonsController();
 
 const app = new Elysia();
-  app
-    .get("/:id", personController.retrievePersonById, retrievePersonByIdSchema)
-    .get("/pessoas", personController.searchPersonsByTerm, searchPersonByTermSchema)
-    .post("/pessoas", personController.createPerson, createPersonSchema)
-    .get("/contagem-pessoas", personController.retrievePersonsCount)
-    .listen(SERVER_PORT as unknown as number);
 
+app
+  .get(
+    '/pessoas/:id',
+    async (context) => {
+      return personController.retrievePersonById(context);
+    },
+    retrievePersonByIdSchema,
+  )
+  .get(
+    '/pessoas',
+    async (context) => {
+      return personController.searchPersonsByTerm(context);
+    },
+    searchPersonByTermSchema,
+  )
+  .post(
+    '/pessoas',
+    async (context) => {
+      return personController.createPerson(context);
+    },
+    createPersonSchema,
+  )
+  .get('/contagem-pessoas', async () => {
+    return personController.retrievePersonsCount();
+  })
+  .onRequest(({ request }) => {
+    console.log(`request: ${request.method} ${request.url}`);
+  });
+
+app.listen(SERVER_PORT);
 console.log(`server running on port ${app.server?.port}`);
-console.log('server routes: ', JSON.stringify(app.routes, null, 2));
